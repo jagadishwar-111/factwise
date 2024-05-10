0a920f961a4e0c4f7d6c33d41db2d28cd4b558fd
jagadishwar-111 <yjagadish123143@gmail.com>
2024-05-10 10:51:55 +0530

    Committing changes in frontend/factwise-app directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter,Route,Routes} from  "react-router-dom"
+import Modal from 'react-modal';
+import ListView from "./Components/ListView"
+import './App.css'
 
-function App() {
+const App = () => {
+
+  Modal.setAppElement('#root');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<ListView/>}/>
+    </Routes>
+
+      </BrowserRouter>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
